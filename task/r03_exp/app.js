a0b947bab7565b5795dbe8de58688b1308bfd17d
sikigasa <km3t1114@yahoo.co.jp>
2023-11-27 17:28:26 +0900

    fix: add change

--- task/r03_exp/app.js
+++ task/r03_exp/app.js
@@ -6,7 +6,8 @@ const app = exp();
 const port = 8083;
 // ルートへのアクセスを処理
 app.get('/', (req, res) => {
-  res.send('Hello World.exp.js');
+  // res.send('Hello World.exp.js');
+  res.sendFile(__dirname + '/public/top.html');
 });
 
 // サーバーを起動
