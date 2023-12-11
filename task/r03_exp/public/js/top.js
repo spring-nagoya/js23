3059fe200ba63bbaac8a4ef1178fd6dd3ee78905
sikigasa <km3t1114@yahoo.co.jp>
2023-12-11 17:08:57 +0900

    add: top.html

--- task/r03_exp/public/js/top.js
+++ task/r03_exp/public/js/top.js
@@ -1,5 +1,18 @@
-$(function(){
-  $("subBtn").click(function(){
+'use strict';
 
-  });
+var $ = require('jquery');
+
+$(function () {
+  $("#btn").on(
+    {
+      'click': function () {
+        alert('ぼたん押した');
+      }
+    },
+    // {
+    //   'mouseover': function () {
+    //     $(this).css('background-color', '#0000ff');
+    //   }
+    // },
+  );
 });
