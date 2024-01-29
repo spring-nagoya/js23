b8b688046e51888e60a527dde42c04c71bee7947
sikigasa <km3t1114@yahoo.co.jp>
2024-01-29 17:22:02 +0900

    update: 授業

--- task/r03_exp/app.js
+++ task/r03_exp/app.js
@@ -22,64 +22,68 @@ const port = 8080;
 app.use(exp.static('public'));
 
 // Web表示
-const server = http.createServer(function (req, res) {
-  
-  if (req.url != '/favicon.ico') {
-    res.setHeader('Content-Type', 'text/plain;charset=UTF-8');
-    // res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello World \n');
+// const server = http.createServer(function (req, res) {
 
-    // connect to mysql
-    const conn = mysql.createConnection({
-      host: '127.0.0.1',
-      port: 3306,
-      user: 'root',
-      password: 'mysql',
-      database: '2023js23db'
-    });
+// if (req.url != '/favicon.ico') {
+// res.setHeader('Content-Type', 'text/plain;charset=UTF-8');
+// res.writeHead(200, {'Content-Type': 'text/html'});
+// res.write('Hello World');
 
-    conn.connect(function (err) {
-      if (err != null) {
-        console.log('Error: ' + err.stack);
-        return;
-      }
-      console.log('Connected as id ' + conn.threadId);
+// connect to mysql
+const conn = mysql.createConnection({
+  host: '127.0.0.1',
+  port: 3306,
+  user: 'root',
+  password: 'Passw0rd',
+  database: '2023js23db'
+});
 
-      const sql = 'SELECT * FROM user';
-      conn.query(sql, (err, results, fields) => {
-        if (err) throw err;
-        console.log(results);
-        if (results.length) {
-          for(let result of results){
-            console.log(result);
-            let tmpl = result.name+ " " +result.age + "歳\n";
-            res.write(tmpl);
-          }
-        }
-        res.end();
-      });
-      conn.end(
-        function (err) {
-          if (err != null) {
-            console.log('Error: ' + err.stack);
-            return;
-          }
-          console.log('Connection closed');
-        });
-    });
+conn.connect(function (err) {
+  if (err != null) {
+    console.log('Error: ' + err.stack);
+    return;
   }
-  // res.end();
-}).listen(8080);
+  console.log('Connected as id ' + conn.threadId);
+
+
+
+});
+// }
+// }).listen(8080);
 
 // // ルートへのアクセスを処理
-// app.get('/', (req, res) => {
-//   // res.send('Hello World.exp.js');
-//   res.sendFile(__dirname + '/public/top.html');
-//   // res.render関数(jsからhtml描画)を使う場合は、
-//   // app.set('view engine', 'ejs');が必要
-// });
+app.get('/', (req, res) => {
+  //   // res.send('Hello World.exp.js');
+  // res.sendFile(__dirname + '/public/top.html');
+  //   // res.render関数(jsからhtml描画)を使う場合は、
+  //   // app.set('view engine', 'ejs');が必要
+  res.setHeader('Content-Type', 'text/plain;charset=UTF-8');
+  const sql = 'SELECT * FROM user';
+  conn.query(sql, (err, results, fields) => {
+    if (err) throw err;
+    if (results.length) {
+      for (let result of results) {
+        console.log(result);
+        let tmpl = result.name+ " " +result.age + "歳\n";
+            res.write(tmpl);
+      }
+      res.end();
+    }
+    
+  });
+});
+
+// conn.end(
+//   function (err) {
+//     if (err != null) {
+//       console.log('Error: ' + err.stack);
+//       return;
+//     }
+//     console.log('Connection closed');
+//   });
+
 
 // // サーバーを起動
-// app.listen(port, () => {
-//   console.log('Start server port: ' + port);
-// });
+app.listen(port, () => {
+  console.log('Start server port: ' + port);
+});
